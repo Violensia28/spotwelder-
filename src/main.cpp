25e54848e90652fd074d496342481636fb66a8b3
Violensia28 <107469569+Violensia28@users.noreply.github.com>
2025-10-01 22:17:29 +0700

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -1,47 +1,62 @@
-#include <Arduino.h>
 #include <WiFi.h>
 #include <WebServer.h>
-#include "ui_assets_min.h"
+#include "Config.h"
 
-#ifndef AP_ONLY
-#define AP_ONLY 1
-#endif
-
-#ifndef DEFAULT_PIN_SSR
-#define DEFAULT_PIN_SSR 26
-#endif
+const char* ssid = "SpotWelder_AP";
+const char* password = "12345678";
 
 WebServer server(80);
 
-static void handleStatic(const char* mime, const char* data){ server.send_P(200, mime, data); }
+// Simple HTML UI with Web Audio beep
+const char htmlPage[] PROGMEM = R"rawliteral(
+<!DOCTYPE html>
+<html>
+<head>
+<title>SpotWelder+</title>
+<script>
+function beep() {
+    var ctx = new (window.AudioContext || window.webkitAudioContext)();
+    var oscillator = ctx.createOscillator();
+    oscillator.type = 'square';
+    oscillator.frequency.setValueAtTime(440, ctx.currentTime);
+    oscillator.connect(ctx.destination);
+    oscillator.start();
+    setTimeout(function(){ oscillator.stop(); }, 200);
+}
+</script>
+</head>
+<body>
+<h1>SpotWelder+ Control</h1>
+<button onclick="fetch('/trigger'); beep();">Trigger Weld</button>
+</body>
+</html>
+)rawliteral";
 
-static void setupRoutes(){
-  server.on("/", []{ handleStatic("text/html; charset=utf-8", INDEX_HTML); });
-  server.on("/css/main.css", []{ handleStatic("text/css", CSS_MAIN); });
-  server.on("/css/theme.css", []{ handleStatic("text/css", CSS_THEME); });
-  server.on("/js/app.js", []{ handleStatic("application/javascript", JS_APP); });
-  server.on("/js/api.js", []{ handleStatic("application/javascript", JS_API); });
-  server.on("/js/audio.js", []{ handleStatic("application/javascript", JS_AUDIO); });
-  server.on("/js/ui.js", []{ handleStatic("application/javascript", JS_UI); });
-  server.on("/js/preset.js", []{ handleStatic("application/javascript", JS_PRESET); });
-  server.on("/js/manual.js", []{ handleStatic("application/javascript", JS_MANUAL); });
-  server.on("/js/status.js", []{ handleStatic("application/javascript", JS_STATUS); });
-  server.on("/js/logs.js", []{ handleStatic("application/javascript", JS_LOGS); });
-  server.on("/js/settings.js", []{ handleStatic("application/javascript", JS_SETTINGS); });
-  server.on("/js/ota.js", []{ handleStatic("application/javascript", JS_OTA); });
-  server.on("/assets/logo.svg", []{ handleStatic("image/svg+xml", SVG_LOGO); });
-  server.on("/ota", []{ handleStatic("text/html; charset=utf-8", OTA_HTML); });
-  server.onNotFound([]{ server.send(404, "text/plain", "404"); });
+void handleRoot() {
+    server.send(200, "text/html", htmlPage);
 }
 
-void setup(){
-  Serial.begin(115200);
-  pinMode(DEFAULT_PIN_SSR, OUTPUT); digitalWrite(DEFAULT_PIN_SSR, LOW);
-  WiFi.mode(WIFI_AP);
-  WiFi.softAP("SPOTWELD+", "weld12345");
-  setupRoutes();
-  server.begin();
-  Serial.println("Ready. Connect to AP SPOTWELD+ and open http://192.168.4.1/");
+void handleTrigger() {
+    digitalWrite(SSR_PIN, HIGH);
+    delay(100); // simulate weld pulse
+    digitalWrite(SSR_PIN, LOW);
+    server.send(200, "text/plain", "Weld triggered");
 }
 
-void loop(){ server.handleClient(); }
+void setup() {
+    Serial.begin(115200);
+    pinMode(SSR_PIN, OUTPUT);
+    digitalWrite(SSR_PIN, LOW);
+
+    WiFi.mode(WIFI_AP);
+    WiFi.softAP(ssid, password);
+    Serial.println("AP started: SpotWelder+");
+
+    server.on("/", handleRoot);
+    server.on("/trigger", handleTrigger);
+    server.begin();
+}
+
+void loop() {
+    server.handleClient();
+}
