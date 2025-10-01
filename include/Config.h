a9e20c0f000e983652e3c24bf2cc0d65662b3673
Violensia28 <107469569+Violensia28@users.noreply.github.com>
2025-10-01 23:58:51 +0700

    Add files via upload

--- include/Config.h
+++ include/Config.h
@@ -9,7 +9,7 @@
 #define PRESET_COUNT 99
 
 #ifndef BUILD_VERSION
-#define BUILD_VERSION "Build_4_Sensor"
+#define BUILD_VERSION "Build_5_AutoGuard"
 #endif
 
 // SoftAP credentials
@@ -23,10 +23,19 @@
 // Sensor defaults (can be calibrated)
 #define DEF_I_OFFSET 2048
 #define DEF_V_OFFSET 2048
-#define DEF_I_SCALE  0.00050f   // A per ADC count (placeholder, to be calibrated)
-#define DEF_V_SCALE  0.10000f   // V per ADC count (placeholder, to be calibrated)
+#define DEF_I_SCALE  0.00050f   // A per ADC count (placeholder)
+#define DEF_V_SCALE  0.10000f   // V per ADC count (placeholder)
 
 // Sensor window (ms)
 #define SENSE_WIN_MS 250
+
+// Auto-Trigger & Guards (defaults)
+#define DEF_AUTO_EN      1   // 1=enable auto-trigger
+#define DEF_GUARD_EN     1   // 1=enable V/I guards
+#define DEF_I_TRIG_A     2.0f   // trigger when Irms >= this (A)
+#define DEF_V_CUTOFF_V   150.0f // permit weld only if Vrms >= this (V)
+#define DEF_I_LIMIT_A    35.0f  // abort weld if Irms > this (A)
+#define DEF_COOLDOWN_MS  1500   // min time between weld cycles
+#define DEF_STABLE_WIN   2      // windows (SENSE_WIN_MS) required to arm trigger
 
 #endif
