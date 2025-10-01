a8cbb23b2d99473b968e918268a32e8e2415ef70
Violensia28 <107469569+Violensia28@users.noreply.github.com>
2025-10-01 22:17:51 +0700

    Add files via upload

---
+++ include/Config.h
@@ -0,0 +1,13 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// Pin mapping
+#define SSR_PIN 26
+#define ACS712_PIN 34
+#define ZMPT_PIN 35
+#define BUZZER_PIN 27 // Dummy, replaced by Web Audio beep
+
+// Constants
+#define PRESET_COUNT 99
+
+#endif
