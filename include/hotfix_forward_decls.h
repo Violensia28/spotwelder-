7aa3f0521b58c553a5d64fe3c331b8244aea2d66
Violensia28 <107469569+Violensia28@users.noreply.github.com>
2025-10-03 12:10:47 +0700

    Delete include/hotfix_forward_decls.h

--- include/hotfix_forward_decls.h
+++
@@ -1,7 +0,0 @@
-
-#ifndef HOTFIX_FORWARD_DECLS_H
-#define HOTFIX_FORWARD_DECLS_H
-// Forward declarations so main.cpp can reference these in setup()
-void smartHistoryJson();
-void smartHistoryCsv();
-#endif
