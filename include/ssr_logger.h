c033f2f84da46971548913a81c0c398da4c2a76e
Violensia28 <107469569+Violensia28@users.noreply.github.com>
2025-10-03 12:32:58 +0700

    Delete include directory

--- include/ssr_logger.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <Arduino.h>
-struct SSRPulseRec{
-  uint32_t t0_ms; int preset_idx; bool dual; uint16_t pre_ms; uint16_t pause_ms; uint16_t main_ms;
-  float i_start_mA; float i_post_mA; float v_start_V; float v_post_V;
-};
-struct SSRLog{ static String dumpCSV(); };
